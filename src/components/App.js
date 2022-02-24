d77efe9aabab95e9e17fd9043a8aadb99adbd51e
Vi Vo <aaronvo118@gmail.com>
2022-02-23 20:57:02 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
   }
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDarkMode ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
